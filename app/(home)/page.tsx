abe8dffda7130f6f4bfbf90cbb21c7d979aee0ef
soloman <soloman.err@gmail>
2024-01-12 18:19:13 +0600

    responsive layout update

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,10 +2,29 @@
 import useContent from '@/hooks/useContent';
 import Image from 'next/image';
 import Masonry from 'react-masonry-css';
+import useSWRInfinite from 'swr/infinite';
+
+const getKey = (pageIndex: number, previousPageData: string | any[]) => {
+  if (previousPageData && !previousPageData.length) return null; // reached the end
+  return `https://api.unsplash.com/search/photos?page=${
+    pageIndex + 1
+  }&query=office&client_id=Ff22f5zprsObQa_a7nGFfHiVja-s2ifGq6BWX3sCvF8`;
+};
 
 export default function Home() {
   const { photos } = useContent();
-  console.log('from homepage', photos);
+  // console.log('from homepage', photos);
+
+  const { data, error, size, setSize } = useSWRInfinite(getKey);
+
+  // const photos = data ? [].concat(...data) : [];
+  console.log(photos);
+  const isLoadingInitialData = !data && !error;
+  const isLoadingMore =
+    isLoadingInitialData ||
+    (size > 0 && data && typeof data[size - 1] === 'undefined');
+  const isEmpty = data?.[0]?.length === 0;
+  const isReachingEnd = isEmpty || (data && data[data.length - 1]?.length < 10);
 
   const breakpointColumnsObj = {
     default: 6,
@@ -21,18 +40,23 @@ export default function Home() {
         className="my-masonry-grid"
         columnClassName="my-masonry-grid_column"
       >
-        {photos?.map((image, index) => (
-          <div key={index}>
-            <Image
-              src={image?.urls?.raw}
-              alt={image?.alt_description}
-              width={600}
-              height={600}
-              key={index}
-              blurDataURL="blur"
-            />
-          </div>
-        ))}
+        {photos?.map(
+          (
+            image: { urls: { raw: string }; alt_description: string },
+            index: number
+          ) => (
+            <div key={index}>
+              <Image
+                src={image?.urls?.raw}
+                alt={image?.alt_description}
+                width={600}
+                height={600}
+                key={index}
+                blurDataURL="blur"
+              />
+            </div>
+          )
+        )}
       </Masonry>
     </main>
   );
