2340a82a2e981c295366608d6f1366e871d43f5b
soloman <soloman.err@gmail>
2024-01-13 15:47:38 +0600

    login form/layout update

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -21,7 +21,7 @@ const Login = () => {
 
   return (
     <div className="flex flex-col justify-center items-center md:min-h-screen">
-      <div className="flex flex-col justify-center items-center max-w-sm w-[90%] shadow-md pt-10 rounded-md mt-32 md:mt-0">
+      <div className="flex flex-col justify-center items-center max-w-sm w-[90%] shadow-md pt-10 rounded-md mt-20 md:mt-0">
         {/* App Info */}
         <div className="flex flex-col justify-center">
           <div className="relative">
